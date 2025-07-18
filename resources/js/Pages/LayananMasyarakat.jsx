d63ba3591fbe018eecf5fd391d1acb1cdb112936
RifqiCah <kuliahfiki@gmail.com>
2025-07-19 02:50:09 +0700

    enhance LayananMasyarakat page with text shadow effects and improved layout for Pembuatan Surat section

--- resources/js/Pages/LayananMasyarakat.jsx
+++ resources/js/Pages/LayananMasyarakat.jsx
@@ -33,7 +33,9 @@ const LayananMasyarakat = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-[92px] items-center">
                       {/* teks  */}
                       <div className="order-2 md:order-1 flex flex-col justify-center">
-                          <h1 className="text-[44px] font-bold text-[#0272BA] mb-6">
+                          <h1 className="text-[44px] font-bold text-[#0272BA] mb-6"
+                          style={{ textShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)' }}
+                          >
                               Selamat datang di Layanan Masyarakat Website Desa Karanganyar!
                           </h1>
 
@@ -103,15 +105,93 @@ const LayananMasyarakat = () => {
               </section>
 
             <section class="min-h-screen w-full bg-white flex items-center">
-            <div h-screen w-full>
-              <h1 className="text-red-400">Pembuatan Surat</h1>
-            </div>
+              <div className="absolute inset-0 w-[10%] h-[30%] rounded-full bg-[#95CFF4] blur-[85px] flex-shrink-0 top-[110%]"></div>
+                  <div class="container mx-auto">
+                    <div className="flex justify-center items-center min-h-auto my-[32px]">
+                      <h1 className="text-[24px] font-bold text-[#0272BA]">Pembuatan Surat</h1>
+                      
+                    </div>
+                    <div className="w-full h-[1px] bg-[#0272BA]/40"></div>
+                    
+                       
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-[94px] items-start mt-[44px]">
+                      {/* teks kiri */}
+                      <div className="order-2 md:order-1 flex flex-col justify-start relative w-[536px]">
+                        {/* Background blur */}
+                        <div className="absolute inset-0 w-full h-auto rounded-full bg-blue-200 blur-[75px] z-9"></div>
+                        
+                        <h1 className="text-[56px] font-bold text-[#0272BA] relative z-10"
+                        style={{ textShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)' }}
+                        >
+                          Pembuatan Surat Desa Karanganyar
+                        </h1>
+                      </div>
+                                            
+                      {/* teks kanan */}
+                      <div className="order-1 md:order-2 flex justify-center items-start relative flex-col max-w-[770px] h-auto">
+                        <p className="text-[24px] font-semibold text-black text-lg">
+                              Selamat datang di bagian Pembuatan Surat Website Desa Karanganyar!<br/><br/>
+                              Kami hadir untuk mempermudah setiap langkah Anda dalam mengurus dan membuat surat yang diperlukan. Di sini, kami menyediakan berbagai layanan pembuatan surat yang dirancang untuk memenuhi kebutuhan administrasi seluruh warga Desa Karanganyar.
+                        </p>
+                        <div className="flex justify-center items-start min-h-auto">
+                          <Link className="" href="/layanan">
+                                <motion.div
+                                  initial={{
+                                    background:
+                                      "linear-gradient(180deg, #0272BA 0%, #95CFF4 98%)",
+                                  }}
+                                  whileHover={{
+                                    background:
+                                      "linear-gradient(180deg, #0272BA 0%, #0272BA 98%)",
+                                  }}
+                                  transition={{ duration: 0.3, easing: "easeIn" }}
+                                  className="text-[20px] rounded-[40px] font-semibold px-[40px] py-[24px] mt-[36px] text-center max-w-fit text-white font-sans opacity-100 transition-all duration-500">
+                                    Klik di sini untuk membuat surat
+                                </motion.div>
+                            </Link>
+                        </div>
+                        <div className="flex justify-center items-start min-h-auto">
+                          <Link className="" href="/layanan">
+                                <motion.div
+                                  initial={{
+                                    background:
+                                      "linear-gradient(180deg, #0272BA 0%, #95CFF4 98%)",
+                                  }}
+                                  whileHover={{
+                                    background:
+                                      "linear-gradient(180deg, #0272BA 0%, #0272BA 98%)",
+                                  }}
+                                  transition={{ duration: 0.3, easing: "easeIn" }}
+                                  className="text-[20px] rounded-[40px] font-semibold px-[40px] py-[24px] mt-[36px] text-center max-w-fit text-white font-sans opacity-100 transition-all duration-500">
+                                    Klik di sini untuk cek status surat
+                                </motion.div>
+                            </Link>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[91px] pt-[56px]">
+                      <div className="space-y-[45px]">
+                        <p className="text-black text-[24px] font-semibold ">1. Surat Keterangan Domisili</p>
+                        <p className="text-black text-[24px] font-semibold ">2. Surat Keterangan Usaha</p>
+                        <p className="text-black text-[24px]  font-semibold">3. Surat Berpergian</p>
+                      </div>
+                      
+                      <div className="space-y-[45px]">
+                        <p className="text-black text-[24px] font-semibold ">4. Surat SKCK</p>
+                        <p className="text-black text-[24px] font-semibold ">5. Surat SKCN</p>
+                        <p className="text-black text-[24px] font-semibold ">6. Surat Keterangan Kerja</p>
+                      </div>
+                      <div className="space-y-[45px]">
+                        <p className="text-black text-[24px] font-semibold ">7. Surat Keterangan Penghasilan</p>
+                        <p className="text-black text-[24px] font-semibold ">8. Surat Keterangan Belum Menikah</p>
+                        <p className="text-black text-[24px] font-semibold ">9. Surat Kelahiran, Surat Kematian</p>
+                      </div>
+                    </div>
+                    {/* Garis pemisah */}
+                      <div className="w-full h-[1px] bg-black/40 mt-[24px]"></div>
+                  </div>
             </section>
-            
-            
-            
-        </>
-    
+        </>    
   )
 }
 
