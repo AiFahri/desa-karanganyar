3592a27de69fceb3c4dab835c4a451754c8c63dd
Royhan-1 <altrammar@gmail.com>
2025-07-18 16:23:02 +0700

    update hero done

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     return (
         <nav className="bg-darkBlue shadow-md sticky top-0 z-50">
-            <div className="container px-4 sm:px-6 lg:px-16">
+            <div className="container px-4 sm:px-6 lg:px-16 min-w-full">
                 <div className="flex items-center justify-between h-auto">
                     {/* Bagian Kiri: Logo */}
                     <div className="flex flex-row justify-between py-2">
@@ -29,7 +29,7 @@ const Navbar = () => {
 
                     {/* Bagian Kanan: Nav Links */}
                     <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
+                        <div className="flex items-center space-x-4">
                             <Link
                                 href="/"
                                 className={`px-3 py-2 rounded-md text-sm font-medium ${
@@ -80,9 +80,18 @@ const Navbar = () => {
                                     route().current('dashboard') 
                                         ? 'text-white' 
                                         : 'text-white hover:bg-gray-200 hover:text-black'
-                                }`}
+                                }`} 
                             >
                                 Portal Berita
+                            </Link>
+
+                            <Link
+                                href={route('dashboard')} // Menggunakan helper route dari Ziggy
+                                className={`px-3 py-2 rounded-md text-sm font-medium `}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45" fill="none">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M22.4999 2.47215e-10C25.4547 -1.38501e-05 28.3805 0.581954 31.1103 1.71268C33.8402 2.8434 36.3206 4.50073 38.4099 6.59004C40.4992 8.67936 42.1565 11.1597 43.2873 13.8896C44.418 16.6194 45 19.5452 45 22.4999C45 34.9264 34.9264 45 22.4999 45C10.0736 45 0 34.9264 0 22.4999C0 10.0736 10.0736 2.47215e-10 22.4999 2.47215e-10ZM24.75 24.75H20.25C14.6797 24.75 9.89739 28.1236 7.83474 32.9394C11.0984 37.5158 16.4507 40.5 22.4999 40.5C28.5492 40.5 33.9015 37.5158 37.1653 32.9391C35.1026 28.1236 30.3203 24.75 24.75 24.75ZM22.4999 6.74999C18.7721 6.74999 15.75 9.77209 15.75 13.5C15.75 17.2279 18.7721 20.25 22.4999 20.25C26.2278 20.25 29.2499 17.2279 29.2499 13.5C29.2499 9.77209 26.2279 6.74999 22.4999 6.74999Z" fill="#D9D9D9"/>
+                                </svg>
                             </Link>
                         </div>
                     </div>
--- resources/js/Pages/LayananMasyarakat.jsx
+++ resources/js/Pages/LayananMasyarakat.jsx
@@ -5,70 +5,76 @@ const LayananMasyarakat = () => {
   return (
         <>
             <Navbar />
-              <section class="min-h-screen w-full bg-white flex items-center px-24">
+              <section class="min-h-screen w-full bg-white flex items-center">
+                <div className="absolute inset-0 w-[10%] h-[30%] rounded-full bg-[#95CFF4] blur-[85px] flex-shrink-0 left-[90%]"></div>
+                <div className="absolute inset-0 w-[10%] h-[30%] rounded-full bg-[#95CFF4] blur-[85px] flex-shrink-0 top-[90%]"></div>
                 
                   <div class="container mx-auto">
                     <div className="flex justify-center items-center min-h-auto my-16">
                       <a
                         href="/layanan"
-                        className="text-[50px] bg-gradient-to-b from-[#0272BA] to-[#95CFF4] text-white font-semibold px-6 py-2 rounded-full shadow-md hover:opacity-90 transition"
+                        className="text-[50px] bg-gradient-to-b from-[#0272BA] to-[#95CFF4] text-white font-semibold px-6 py-2 rounded-full shadow-md hover:bg-[blue] transition"
                       >
                         Layanan Masyarakat
                       </a>
                     </div>
                        
-                      <div class="grid grid-cols-1 md:grid-cols-2 gap-[92px] items-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-[92px] items-center">
+                      {/* teks  */}
+                      <div className="order-2 md:order-1 flex flex-col justify-center">
+                          <h1 className="text-[44px] font-bold text-[#0272BA] mb-6">
+                              Selamat datang di Layanan Masyarakat Website Desa Karanganyar!
+                          </h1>
 
-                          {/* teks  */}
-                          <div class="order-2 md:order-1">
-                              <h1 class="text-[44px] font-bold text-[#0272BA] mb-6">
-                                Selamat datang di Layanan Masyarakat Website Desa Karanganyar!
-                              </h1>
-
-                              <p class="text-[24px] font-bold text-black text-lg mb-6">
-                                  Kami hadir untuk mempermudah setiap langkah Anda dalam pengajuan dan pembuatan surat yang dibutuhkan. Melalui layanan ini, kami menyediakan fitur Pembuatan Surat untuk memudahkan administrasi seluruh warga Desa Karanganyar.
-                              </p>
-
-                          </div>
-                          
-                          {/* gambar */}
-                          <div class="order-1 md:order-2">
-                                <img src="/LayananMasyarakat/Layanan1.png" alt="Gambar layanan" />
-                          </div>
+                          <p className="text-[24px] font-bold text-black text-lg mb-6">
+                              Kami hadir untuk mempermudah setiap langkah Anda dalam pengajuan dan pembuatan surat yang dibutuhkan. Melalui layanan ini, kami menyediakan fitur Pembuatan Surat untuk memudahkan administrasi seluruh warga Desa Karanganyar.
+                          </p>
                       </div>
+                    
+                      {/* gambar */}
+                      <div className="order-1 md:order-2 flex items-center justify-center relative ">
+                          {/* Background blur - ukuran diperkecil dan diposisikan ulang */}
+                          <div className="absolute inset-0 w-full h-auto rounded-full bg-blue-100 blur-[75px] flex-shrink-0"></div>
+                          <img 
+                              src="/LayananMasyarakat/Layanan1.png" 
+                              alt="Gambar layanan" 
+                              className="relative z-10 max-w-full h-auto"
+                          />
+                      </div>
+                    </div>
 
-                <div class="max-w-6xl mx-auto">
-                        <div class="flex flex-wrap justify-center gap-8">
-                            {/* <!-- Card 1 - Cepat --> */}
-                            <div class="bg-transparant rounded-2xl p-8 text-center shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 max-w-xs w-full min-h-[320px] flex flex-col items-center">
-                            <div class="w-20 h-20 rounded-full bg-red-100 flex items-center justify-center mb-6 flex-shrink-0">
-                                <img src="/LayananMasyarakat/albums.svg" alt="Speed Icon" class="w-10 h-10" />
-                            </div>
-                                <h3 class="text-xl font-semibold text-gray-800 mb-4">Cepat</h3>
-                                <p class="text-sm text-gray-600 leading-relaxed text-justify flex-grow">
-                                    Proses pembayaran yang cepat menggunakan teknologi terdepan, tanpa perlu antri atau menunggu lama. Semua transaksi dapat diselesaikan secara online dengan mudah dan efisien.
+                    <div className="max-w-full mx-auto my-[83px]">
+                        <div className="flex flex-wrap justify-center gap-[52px]">
+                            {/* Card 1 - Cepat */}
+                            <div className="bg-transparent rounded-2xl p-8 text-center w-[395px] min-h-auto transition-all duration-300 hover:-translate-y-2 flex flex-col items-center">
+                                <div className="w-20 h-20 rounded-full bg-red-500 flex items-center justify-center mb-6 flex-shrink-0">
+                                    <img src="/LayananMasyarakat/albums.svg" alt="Speed Icon" className="w-10 h-10" />
+                                </div>
+                                <h3 className="text-[24px] font-semibold text-black mb-4">Cepat</h3>
+                                <p className="text-[18px] text-black leading-relaxed text-justify flex-grow">
+                                  Proses pengajuan surat menjadi lebih singkat tanpa perlu antre di kantor desa. Semua bisa Anda lakukan secara online, kapan saja sesuai kebutuhan.
                                 </p>
                             </div>
 
-                            {/* <!-- Card 2 - Mudah --> */}
-                            <div class="bg-white rounded-2xl p-8 text-center shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 max-w-xs w-full min-h-[320px] flex flex-col items-center">
-                                <div class="w-20 h-20 rounded-full border-2 border-dashed border-yellow-400 bg-yellow-50 flex items-center justify-center text-sm text-gray-600 mb-6 flex-shrink-0">
-                                    <img src="/LayananMasyarakat/albums.svg" alt="Speed Icon" class="w-10 h-10" />
+                            {/* Card 2 - Mudah */}
+                            <div className="bg-transparent rounded-2xl p-8 text-center w-[395px] min-h-auto transition-all duration-300 hover:-translate-y-2  flex flex-col items-center">
+                                <div className="w-20 h-20 rounded-full bg-yellow-500 flex items-center justify-center mb-6 flex-shrink-0">
+                                    <img src="/LayananMasyarakat/albums.svg" alt="Easy Icon" className="w-10 h-10" />
                                 </div>
-                                <h3 class="text-xl font-semibold text-gray-800 mb-4">Mudah</h3>
-                                <p class="text-sm text-gray-600 leading-relaxed text-justify flex-grow">
-                                    Formulir dan alur pengajuan peminjaman yang mudah dipahami. Tidak perlu dokumen rumit atau persyaratan yang dapat membingungkan. Sistem dengan mudah, tanpa ribet dan berbelit-belit.
+                                <h3 className="text-[24px] font-semibold text-black mb-4">Mudah</h3>
+                                <p className="text-[18px] text-black leading-relaxed text-justify flex-grow">
+                                    Formulir dan alur pengajuan dirancang sederhana dan ramah pengguna, sehingga siapa pun dapat mengurus surat dengan mudah, bahkan bagi yang baru pertama kali mencoba.
                                 </p>
                             </div>
 
-                            {/* <!-- Card 3 - Hemat Waktu --> */}
-                            <div class="bg-white rounded-2xl p-8 text-center shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-2 max-w-xs w-full min-h-[320px] flex flex-col items-center">
-                                <div class="w-20 h-20 rounded-full border-2 border-dashed border-blue-400 bg-blue-50 flex items-center justify-center text-sm text-gray-600 mb-6 flex-shrink-0">
-                                    <img src="/LayananMasyarakat/albums.svg" alt="Speed Icon" class="w-10 h-10" />
+                            {/* Card 3 - Hemat Waktu */}
+                            <div className="bg-transparent rounded-2xl p-8 text-center w-[395px] min-h-auto transition-all duration-300 hover:-translate-y-2 flex flex-col items-center">
+                                <div className="w-20 h-20 rounded-full bg-blue-600 flex items-center justify-center mb-6 flex-shrink-0">
+                                    <img src="/LayananMasyarakat/albums.svg" alt="Time Icon" className="w-10 h-10" />
                                 </div>
-                                <h3 class="text-xl font-semibold text-gray-800 mb-4">Hemat Waktu</h3>
-                                <p class="text-sm text-gray-600 leading-relaxed text-justify flex-grow">
-                                    Tidak perlu menunggu lama untuk mendapatkan persetujuan. Proses verifikasi dan persetujuan dalam hitungan menit. Waktu adalah penting, itulah mengapa kami berkomitmen untuk memberikan layanan terbaik.
+                                <h3 className="text-[24px] font-semibold text-black mb-4">Hemat Waktu</h3>
+                                <p className="text-[18px] text-black leading-relaxed text-justify flex-grow">
+                                    Tanpa perlu datang langsung ke kantor desa, Anda dapat mengajukan surat hanya dalam beberapa langkah. Waktu Anda pun dapat dimanfaatkan untuk kegiatan lain yang lebih penting.
                                 </p>
                             </div>
                         </div>
