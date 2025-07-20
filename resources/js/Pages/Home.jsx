6262ded680043c3431442daeaf824b2db9779924
Royhan-1 <altrammar@gmail.com>
2025-07-20 07:23:35 +0700

    add PetaRBI desa + fix CardView deskripsi desa

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
 import CardViewDesa from "@/Components/CardViewDesa";
+import PetaRBI from "@/Components/PetaRBi";
+import bgCardView from "../../assets/img/bgCardView.png";
+import logoCardView from "../../assets/img/logoCardView.png"; 
+import bgPetaView from '../../assets/img/bgPetaView.png';     // Ganti dengan path gambar background kamu
 
 
 const Home = () => {
@@ -9,7 +13,8 @@ const Home = () => {
         <>
             <Navbar />
             <Hero />
-            <CardViewDesa />
+            <CardViewDesa background={bgCardView} logo={logoCardView}/>
+            <PetaRBI backgroundIMG={bgPetaView} />
             {/* <h1 className="text-red-400">Halo brok</h1> */}
             {/* <div className="min-h-[1920px] bg-black block">asd</div> */}
             
