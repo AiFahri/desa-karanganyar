22addcf26a58dfd284f0e74d40dee8d5f6db6b1b
Royhan-1 <altrammar@gmail.com>
2025-07-21 00:08:08 +0700

    Header Potensi done

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -8,6 +8,7 @@ import bgCardView from "../../assets/Home/img/bgCardView.png";
 import logoCardView from "../../assets/Home/img/logoCardView.png"; 
 import bgPetaView from '../../assets/Home/img/bgPetaView.png';     // Ganti dengan path gambar background kamu
 import BuatLayanan from "@/Components/BuatLayanan";
+import HeaderPotensi from "@/Components/HeaderPotensi";
 
 
 const Home = () => {
@@ -18,6 +19,7 @@ const Home = () => {
             <CardViewDesa background={bgCardView} logo={logoCardView}/>
             <PetaRBI backgroundIMG={bgPetaView} />
             <BuatLayanan />
+            <HeaderPotensi />
             {/* <h1 className="text-red-400">Halo brok</h1> */}
             {/* <div className="min-h-[1920px] bg-black block">asd</div> */}
             <Footer />
