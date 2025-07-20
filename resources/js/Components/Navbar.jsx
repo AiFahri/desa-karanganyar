2eef4498be0cf560c3950329048bef386c88c115
Royhan-1 <altrammar@gmail.com>
2025-07-20 23:30:34 +0700

    Add headerPotensi + fix navbar active

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -38,14 +38,16 @@ const Navbar = () => {
                             <Link
                                 href="/"
                                 className={`px-3 py-2 rounded-md text-sm font-medium group ${
-                                    isActive("/")
-                                        ? "text-white"
-                                        : "text-white"
+                                    isActive("/") ? "text-white" : "text-white"
                                 }`}
                             >
                                 <div>Beranda</div>
                                 <div
-                                    className={`mx-auto mb-0 mt-1 w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full h-0 transition-all duration-300`}
+                                    className={`mx-auto mb-0 mt-1 h-0 transition-all duration-300 ${
+                                        isActive("/")
+                                            ? "w-full border-b-[3px] border-white rounded-full" // Active state
+                                            : "w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full" // Inactive and hover state
+                                    }`}
                                 ></div>
                             </Link>
 
@@ -59,7 +61,11 @@ const Navbar = () => {
                             >
                                 <div>Profil Desa</div>
                                 <div
-                                    className={`mx-auto mb-0 mt-1 w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full h-0 transition-all duration-300`}
+                                    className={`mx-auto mb-0 mt-1 h-0 transition-all duration-300 ${
+                                        isActive("/profildesa")
+                                            ? "w-full border-b-[3px] border-white rounded-full"
+                                            : "w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full"
+                                    }`}
                                 ></div>
                             </Link>
 
@@ -67,13 +73,17 @@ const Navbar = () => {
                                 href="/layanan"
                                 className={`px-3 py-2 rounded-md text-sm font-medium group ${
                                     isActive("/layanan")
-                                        ? "text-white border-white"
+                                        ? "text-white"
                                         : "text-white"
                                 }`}
                             >
                                 <div>Layanan Masyarakat</div>
                                 <div
-                                    className={`mx-auto mb-0 mt-1 w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full h-0 transition-all duration-300`}
+                                    className={`mx-auto mb-0 mt-1 h-0 transition-all duration-300 ${
+                                        isActive("/layanan")
+                                            ? "w-full border-b-[3px] border-white rounded-full"
+                                            : "w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full"
+                                    }`}
                                 ></div>
                             </Link>
 
@@ -87,7 +97,11 @@ const Navbar = () => {
                             >
                                 <div className="">Potensi Desa</div>
                                 <div
-                                    className={`mx-auto mb-0 mt-1 w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full h-0 transition-all duration-300`}
+                                    className={`mx-auto mb-0 mt-1 h-0 transition-all duration-300 ${
+                                        route().current("dashboard")
+                                            ? "w-full border-b-[3px] border-white rounded-full"
+                                            : "w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full"
+                                    }`}
                                 ></div>
                             </Link>
 
@@ -101,7 +115,11 @@ const Navbar = () => {
                             >
                                 <div>Portal Berita</div>
                                 <div
-                                    className={`mx-auto mb-0 mt-1 w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full h-0 transition-all duration-300`}
+                                    className={`mx-auto mb-0 mt-1 h-0 transition-all duration-300 ${
+                                        route().current("")
+                                            ? "w-full border-b-[3px] border-white rounded-full"
+                                            : "w-0 border-b-[3px] rounded-full group-hover:border-white group-hover:w-full"
+                                    }`}
                                 ></div>
                             </Link>
 
@@ -117,8 +135,8 @@ const Navbar = () => {
                                     fill="none"
                                 >
                                     <path
-                                        fill-rule="evenodd"
-                                        clip-rule="evenodd"
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
                                         d="M22.4999 2.47215e-10C25.4547 -1.38501e-05 28.3805 0.581954 31.1103 1.71268C33.8402 2.8434 36.3206 4.50073 38.4099 6.59004C40.4992 8.67936 42.1565 11.1597 43.2873 13.8896C44.418 16.6194 45 19.5452 45 22.4999C45 34.9264 34.9264 45 22.4999 45C10.0736 45 0 34.9264 0 22.4999C0 10.0736 10.0736 2.47215e-10 22.4999 2.47215e-10ZM24.75 24.75H20.25C14.6797 24.75 9.89739 28.1236 7.83474 32.9394C11.0984 37.5158 16.4507 40.5 22.4999 40.5C28.5492 40.5 33.9015 37.5158 37.1653 32.9391C35.1026 28.1236 30.3203 24.75 24.75 24.75ZM22.4999 6.74999C18.7721 6.74999 15.75 9.77209 15.75 13.5C15.75 17.2279 18.7721 20.25 22.4999 20.25C26.2278 20.25 29.2499 17.2279 29.2499 13.5C29.2499 9.77209 26.2279 6.74999 22.4999 6.74999Z"
                                         fill="#D9D9D9"
                                     />
