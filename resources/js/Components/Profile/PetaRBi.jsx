93f1438a9f48c900f28668e23d0946217b908fa0
AiFahri <fahrinurilll@gmail.com>
2025-07-29 14:55:38 +0700

    fix(Profile): update image source for PetaRBI component

--- resources/js/Components/Profile/PetaRBi.jsx
+++ resources/js/Components/Profile/PetaRBi.jsx
@@ -34,7 +34,7 @@ const PetaRBI = ({}) => {
 
           <div className="flex w-full">
             <img 
-              src={imgpetaRBI} 
+              src="/images/PETARBI.jpg"
               className='w-full max-w-full h-auto object-contain pt-4 md:pt-6 lg:pt-[24px] max-h-[50vh] md:max-h-[60vh] lg:max-h-[70vh]'
               alt="Peta RBI"
             />
