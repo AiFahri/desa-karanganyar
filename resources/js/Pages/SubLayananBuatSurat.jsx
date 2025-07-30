78e551c1a2a5dcd6fc5a3e3bd155a2c494f6aefe
AiFahri <fahrinurilll@gmail.com>
2025-07-31 05:39:47 +0700

    fix(SubLayananBuatSurat, SubLayananStatusSurat): pass user data to Navbar component

--- resources/js/Pages/SubLayananBuatSurat.jsx
+++ resources/js/Pages/SubLayananBuatSurat.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "@/Components/Navbar";
 import BuatSurat from "@/Components/Layanan/BuatSurat";
 import Footer from "@/Components/Footer";
+import { usePage } from "@inertiajs/react";
 
 const SubLayananBuatSurat = ({ suratJenis }) => {
+    const { auth } = usePage().props;
     return (
         <>
-            <Navbar />
+            <Navbar user={auth.user}/>
             <BuatSurat suratJenis={suratJenis} />
             <Footer />
         </>
--- resources/js/Pages/SubLayananStatusSurat.jsx
+++ resources/js/Pages/SubLayananStatusSurat.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { circInOut, easeInOut, motion } from "framer-motion";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
 import View from "@/Components/Layanan/View";
 import StatusSurat from "@/Components/Layanan/StatusSurat";
 
 const SubLayananStatusSurat = () => {
+    const { auth } = usePage().props;
     return (
         <>
-            <Navbar />
+            <Navbar user={auth.user}/>
             <StatusSurat />
         </>
     );
