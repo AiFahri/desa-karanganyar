e11b645cf8ee3bd7a74ad25b4305345bc54ac1f5
AiFahri <fahrinurilll@gmail.com>
2025-09-16 15:52:43 +0700

    feat: update sitemap structure to include dynamic static pages and improve last modified date format

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,60 +1,59 @@
 {!! '<'.'?xml version="1.0" encoding="UTF-8"?'.'>' !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>{{ url('/') }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>{{ url('/profil') }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/layanan') }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/portal') }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/pengumuman') }}</loc>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-    </url>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     
+    {{-- Static Pages --}}
+    @foreach ($staticPages as $page)
+    <url>
+        <loc>{{ $page['url'] }}</loc>
+        <lastmod>{{ $page['lastmod'] }}</lastmod>
+        <changefreq>{{ $page['changefreq'] }}</changefreq>
+        <priority>{{ $page['priority'] }}</priority>
+    </url>
+    @endforeach
+    
+    {{-- Berita --}}
     @foreach ($berita as $item)
     <url>
         <loc>{{ url('/berita/' . $item->slug) }}</loc>
-        <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
+        <lastmod>{{ $item->updated_at->toISOString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.7</priority>
+        @if($item->gambar)
+        <image:image>
+            <image:loc>{{ url('/storage/' . $item->gambar) }}</image:loc>
+            <image:title>{{ $item->judul }}</image:title>
+            <image:caption>{{ strip_tags($item->deskripsi) }}</image:caption>
+        </image:image>
+        @endif
     </url>
     @endforeach
     
+    {{-- Pengumuman --}}
     @foreach ($pengumuman as $item)
     <url>
         <loc>{{ url('/pengumuman/' . $item->slug) }}</loc>
-        <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
+        <lastmod>{{ $item->updated_at->toISOString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.7</priority>
     </url>
     @endforeach
     
+    {{-- UMKM --}}
     @foreach ($umkm as $item)
     <url>
         <loc>{{ url('/sub-umkm/' . $item->slug) }}</loc>
-        <lastmod>{{ $item->updated_at->toAtomString() }}</lastmod>
+        <lastmod>{{ $item->updated_at->toISOString() }}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.6</priority>
+        @if($item->gambar_path)
+        <image:image>
+            <image:loc>{{ 'https://is3.cloudhost.id/karanganyar/' . $item->gambar_path }}</image:loc>
+            <image:title>{{ $item->merk_dagang }}</image:title>
+            <image:caption>{{ $item->deskripsi_singkat }}</image:caption>
+        </image:image>
+        @endif
     </url>
     @endforeach
 </urlset>
